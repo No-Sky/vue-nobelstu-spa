bc1ede8bee3cf39c4ae3a3f7d0f231e413aff58c
No-Sky <1987545063@qq.com>
2018-08-16 08:16:43 +0800

    update

--- src/components/Recommed.vue
+++ src/components/Recommed.vue
@@ -2,17 +2,8 @@
   <div>
     <!--轮播图-->
     <mu-carousel transition="fade" style="width:100%;height: 200px;" >
-      <mu-carousel-item>
-        <img :src="carouselImg1">
-      </mu-carousel-item>
-      <mu-carousel-item>
-        <img :src="carouselImg2">
-      </mu-carousel-item>
-      <mu-carousel-item>
-        <img :src="carouselImg3">
-      </mu-carousel-item>
-      <mu-carousel-item>
-        <img :src="carouselImg4">
+      <mu-carousel-item v-for="(ad, index) in ads" :key="index">
+        <img :src="ad">
       </mu-carousel-item>
     </mu-carousel>
 
@@ -47,10 +38,12 @@
     name: 'Recommend',
     data () {
       return {
-        carouselImg1,
-        carouselImg2,
-        carouselImg3,
-        carouselImg4,
+        ads: [
+          carouselImg1,
+          carouselImg2,
+          carouselImg3,
+          carouselImg4
+        ],
         list: [{
           teacherid: 0,
           teachername: 'awdawd',
@@ -85,6 +78,18 @@
           if(res.data){
             this.list = res.data;
           }
+        }).error(err => {
+          this.$toast.warning("推荐列表加载失败");
+        })
+      },
+      getAds: function () {
+        this.$http.get(this.$api.ads).then(res => {
+          console.log(res.data);
+          if(res.data){
+            this.ads = res.data;
+          }
+        }).error(err => {
+          this.$toast.warning("广告加载失败");
         })
       },
       toTeacherinfo: function (teacherid) {
